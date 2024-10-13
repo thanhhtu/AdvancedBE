d2f84bd62dd2d90150a3cfc52438f59e8b321065
Thanh Tu <thanhthanhvava2004@gmail.com>
2024-10-13 19:42:03 +0700

    add feat_rbac

--- src/apis/rbac/rbac.router.ts
+++ src/apis/rbac/rbac.router.ts
@@ -14,27 +14,5 @@ route.get('/permissions/:roleName', rbacController.getPermissionsOfRole);
 //get permissions of a user
 route.get('/permissions/user/:id', validateMiddleware.checkNumberParam, rbacController.getPermissionsOfUser);
 
-/*
-//CREATE ROLE
-router.post('/', asyncHandler(RoleController.CreateRole))
-
-//CREATE PERMISSION
-router.post('/permission', asyncHandler(RoleController.CreatePermission))
-
-//ASSIGN ROLE TO USER
-router.post('/assign', asyncHandler(RoleController.AssignRoletoUser))
-
-//ASSIGN PERMISSION TO ROLE
-router.post('/permission/assign', asyncHandler(RoleController.AssignPermissiontoRole))
-
-//DELETE PERMISSION FROM ROLE
-router.delete('/permission-of-role', asyncHandler(RoleController.DeletePermissionfromRole))
-
-//DELETE PERMISSION
-router.delete('/permission', asyncHandler(RoleController.DeletePermission))
-
-//DELETE ROLE
-router.delete('/', asyncHandler(RoleController.DeleteRole))
-*/
 
 export default route;
